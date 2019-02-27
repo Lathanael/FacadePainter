bb9dc4d9d0b7ee93137fbde29113f4bc43cd983f
Lathanael <lathanael@lathanael.de>
2019-02-27 20:34:45 +0100

    Slight cleanup

--- src/main/java/de/lathanael/facadepainter/FacadePainter.java
+++ src/main/java/de/lathanael/facadepainter/FacadePainter.java
@@ -10,10 +10,6 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import org.apache.logging.log4j.Logger;
 
-//import crazypants.enderio.base.recipe.IMachineRecipe;
-//import crazypants.enderio.base.recipe.MachineRecipeRegistry;
-//import crazypants.enderio.base.recipe.painter.AbstractPainterTemplate;
-
 @Mod(modid = FacadePainter.MODID, name = FacadePainter.NAME, version = FacadePainter.VERSION, dependencies = "required-after:forge@[14.23.4.2806,)", useMetadata = true)
 public class FacadePainter {
 
@@ -44,9 +40,4 @@ public class FacadePainter {
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
-
-//    @Mod.EventHandler
-//    public void serverLoad(FMLServerStartingEvent event) {
-//        event.registerServerCommand(new TeleportCommand());
-//    }
 }
